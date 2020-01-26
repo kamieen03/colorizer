3067ea64284b8a91e8bd898f60dbe647a33487c7
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2020-01-26 15:55:51 +0100

    add test

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 from torchsummary import summary
-from generator import UNet
-from discriminator import PatchDiscriminator
+import torch
 import sys
+from PIL import Image
+from colorizer import Colorizer
+import numpy as np
 
+c = Colorizer()
+c.load_state_dict(torch.load('colorizer.pth'))
+img = np.asarray(Image.open('cat.jpg'))
+A = c.rgb2Lab(img)[:1,:,:].unsqueeze(0)
+with torch.no_grad():
+    B = c(A)
+    d = c.netD(torch.cat([A, B], 1))
+print(d, d.shape)
+grey = B[0][2].numpy()*255
+Image.fromarray(grey).show()
+rgb_B = c.Lab2rgb(B)
+Image.fromarray(rgb_B).show()
 
-
-net = UNet().cuda()
-summary(net, (1, int(sys.argv[1]), int(sys.argv[2])))
-net2 = PatchDiscriminator().cuda()
-summary(net2, (3, int(sys.argv[1]), int(sys.argv[2])))
